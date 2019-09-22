45feadf18155933fd22ea0f17e67f18dfc5fcb0c
SoberNT <soberNT@yandex.ru>
2019-09-22 21:17:42 +0700

    fix mobile view

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,14 +8,12 @@ use yii\widgets\LinkPager; ?>
 <div class="site-index">
     <div class="body-content">
             <nav class="navbar navbar-default" role="navigation">
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
             <?php foreach ($categories as $category){
                 echo $this->render('../category/link', compact('category','category_id'));
             }
             ?>
                     </ul>
-                </div>
             </nav>
         <div class="row">
             <div class="body-content col-md-12">
